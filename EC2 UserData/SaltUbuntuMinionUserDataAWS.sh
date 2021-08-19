1bde0b728af89770ec701aac64b9cb33e6837e5f
Josh Higginson <65719561+joshuahigginson1@users.noreply.github.com>
2021-08-19 17:17:40 +0100

    Update and rename SaltUbuntuMinionUserDataAWS.sh to EC2 UserData/SaltUbuntuMinionUserDataAWS.sh

--- EC2 UserData/SaltUbuntuMinionUserDataAWS.sh
+++ EC2 UserData/SaltUbuntuMinionUserDataAWS.sh
@@ -2,6 +2,12 @@
 # This script is used for configuring an x86 Ubuntu machine as a salt master through user data.
 # Requires the urls: repo.saltstack.com and repo.saltproject.io to be allowed on a URL Allowlist.
 
+# User Set Variables
+
+# Automated Variables
+
+SALTMASTERPRIVATEIP = "127.0.0.1"  # TODO: Set with Terraform template.
+
 # If behind corperate proxy...
 
 #if <expression for checking>;
@@ -13,6 +19,10 @@
 curl -fsSL -o /usr/share/keyrings/salt-archive-keyring.gpg https://repo.saltproject.io/py3/ubuntu/20.04/amd64/latest/salt-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/salt-archive-keyring.gpg arch=amd64] https://repo.saltproject.io/py3/ubuntu/20.04/amd64/latest focal main" | sudo tee /etc/apt/sources.list.d/salt.list
 
-# Install Salt-Master.
+# Install Salt-Minion.
 apt update && apt upgrade -y
 apt install salt-minion -y
+
+# Configure Salt Minion.
+sed -i "s/#master: salt/master: $SALTMASTERPRIVATEIP/g" /etc/salt/minion
+service salt-minion restart
