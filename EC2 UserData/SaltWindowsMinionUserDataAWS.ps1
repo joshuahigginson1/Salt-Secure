151dc30420a1694c6dca4d16966f4c6a8da6ca6b
Josh Higginson <65719561+joshuahigginson1@users.noreply.github.com>
2021-08-19 17:34:40 +0100

    Update SaltWindowsMinionUserDataAWS.ps1

--- EC2 UserData/SaltWindowsMinionUserDataAWS.ps1
+++ EC2 UserData/SaltWindowsMinionUserDataAWS.ps1
@@ -11,19 +11,18 @@ $BehindProxy = "True"  # Set to True if working behind a proxy.
 $ProxyAddressAndPort = ""  # Set in the format my.web.proxy:0000 where 0000 is the port number.
 $ProxyOverride = "localhost;127.0.0.1;10.*;.eu-west-2.amazonaws.com;169.254.169.254;169.254.169.253;169.254.169.123;<local>" 
 
-
 # Automatic Variables
 $SaltSource = "https://repo.saltstack.com/windows/$saltversion"
 $SaltDestination = "c:\Salt\$saltversion"
+$SaltMasterPrivateIP = "127.0.0.1"  # TODO: Set with Terraform Template.
 
 ### Code ###
 
-if ($BehindProxy = "True") {
-    Set-ItemProperty "HKCU:\Software\Windows\CurrentVersion\Internet Settings" -name AutoDetect -value 1
-    Set-ItemProperty "HKCU:\Software\Windows\CurrentVersion\Internet Settings" -name ProxyEnable -value 1
-    Set-ItemProperty "HKCU:\Software\Windows\CurrentVersion\Internet Settings" -name ProxyServer -value $ProxyAddressAndPort
-    Set-ItemProperty "HKCU:\Software\Windows\CurrentVersion\Internet Settings" -name ProxyOverride -value $ProxyOverride
-    
+if ($BehindProxy -eq "True") {
+    Set-ItemProperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' -name AutoDetect -value 1
+    Set-ItemProperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' -name ProxyEnable -value 1
+    Set-ItemProperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' -name ProxyServer -value $ProxyAddressAndPort
+    Set-ItemProperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' -name ProxyOverride -value $ProxyOverride
 }
 
 # Configure Windows Time Service
@@ -34,8 +33,9 @@ Install-PackageProvider NuGet -Force
 Install-Package Python3 -Force
 
 # Install Salt Minion
+New-Item -Path c:\temp -ItemType directory
 Set-Location C:\temp
 wget $SaltSource -OutFile c:\temp\$saltversion
-iex '$SaltDestination /S /master=salt-master /minion-name=$env:computername'
+iex "$SaltDestination /S /master=$SaltMasterPrivateIP /minion-name=$env:computername"
 
 </powershell>
